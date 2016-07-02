4064a8a39c66da0aec29687880963d65ae41b245
Re4son <Re4son@users.noreply.github.com>
2016-07-02 13:57:29 +1000

    Add background

--- launch-bg.sh
+++ launch-bg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-BACKGROUND="Kali-Pi-2.8.jpg"
+BACKGROUND="Kali-Pi-3.5.jpg"
 setterm -term linux -back default -fore black -clear all
 fbi -t 2 -1 -d /dev/fb1 -noverbose -a $MENUDIR$BACKGROUND
