8f2f2b77efc79d0af8a93f5e295234cadb5ef15a
Re4son Protagonist <Re4son@users.noreply.github.com>
2016-07-02 14:29:34 +1000

    Adjust menu-1  to 3.5"

--- menu-1.py
+++ menu-1.py
@@ -19,17 +19,17 @@ def make_button(text, xpo, ypo, height, width, colour):
     pygame.draw.rect(screen, tron_regular, (xpo-10,ypo-10,width,height),3)
     pygame.draw.rect(screen, tron_light, (xpo-9,ypo-9,width-1,height-1),1)
     pygame.draw.rect(screen, tron_regular, (xpo-8,ypo-8,width-2,height-2),1)
-    font=pygame.font.Font(None,30)
+    font=pygame.font.Font(None,42)
     label=font.render(str(text), 1, (colour))
-    screen.blit(label,(xpo,ypo+6))
+    screen.blit(label,(xpo,ypo))
 
 def make_enabled_button(text, xpo, ypo, height, width, colour):
     pygame.draw.rect(screen, (colour), (xpo-10,ypo-10,width,height),3)
     pygame.draw.rect(screen, (colour), (xpo-9,ypo-9,width-1,height-1),1)
     pygame.draw.rect(screen, (colour), (xpo-8,ypo-8,width-2,height-2),1)
-    font=pygame.font.Font(None,30)
+    font=pygame.font.Font(None,42)
     label=font.render(str(text), 1, (colour))
-    screen.blit(label,(xpo,ypo+6))
+    screen.blit(label,(xpo,ypo))
 
 
 # define function for printing text in a specific place with a specific colour
@@ -191,17 +191,17 @@ pi_hostname = "  " + pi_hostname[:-1]
 # First Row Label
 make_label(pi_hostname, 32, 30, 48, tron_inverse)
 # Second Row buttons 1 and 2
-make_button("      X-TFT", 30, 105, 55, 210, tron_light)
-make_button("    X-HDMI", 260, 105, 55, 210, tron_light)
+make_button("    X on TFT", 30, 105, 55, 210, tron_light)
+make_button("   X on HDMI", 260, 105, 55, 210, tron_light)
 # Third Row buttons 3 and 4
-make_button("   Terminal", 30, 180, 55, 210, tron_light)
+make_button("    Terminal", 30, 180, 55, 210, tron_light)
 if check_service("apache2"):
      make_enabled_button("     DV-Pi", 260, 180, 55, 210, red)
 else:
      make_button("     DV-Pi", 260, 180, 55, 210, tron_light)
 # Fourth Row Buttons
-make_button("     TFT Off", 30, 255, 55, 210, tron_light)
-make_button("         >>>", 260, 255, 55, 210, tron_light)
+make_button("   Screen Off", 30, 255, 55, 210, tron_light)
+make_button("          >>>", 260, 255, 55, 210, tron_light)
 
 
 #While loop to manage touch screen inputs
